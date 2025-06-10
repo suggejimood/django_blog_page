14a934efb873cd9ec5d1e901e2ea93b20224ea04
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-11 00:56:30 +0300

    feat: Urls & Views added.

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("/", views.index, name="index-page"),
+    path("/posts", views.posts, name="posts-page"),
+    path("post/<slug:slug>", views.post, name="post-page"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return None
+
+def posts(request):
+    pass
+
+def post(request):
+    pass
