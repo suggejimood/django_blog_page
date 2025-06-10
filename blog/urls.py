5272173e0155b065ab394ca90afc8f1d47acc7e4
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-11 01:06:50 +0300

    fix: Wrong path way change

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/", views.index, name="index-page"),
+    path("", views.index, name="index-page"),
     path("/posts", views.posts, name="posts-page"),
     path("post/<slug:slug>", views.post, name="post-page"),
 ]
