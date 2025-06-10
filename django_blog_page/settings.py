a1b1f144fd12b4e5611299a9491ea0153a012911
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-11 01:19:50 +0300

    feat: First templates added.

--- django_blog_page/settings.py
+++ django_blog_page/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'django_blog_page.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
