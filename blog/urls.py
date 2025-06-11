018a5f06e23e2ec06bf5d649db7e0beb20f142c0
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-11 17:51:48 +0300

    fix: wrong posts path way change

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index-page"),
-    path("/posts", views.posts, name="posts-page"),
+    path("posts", views.posts, name="posts-page"),
     path("post/<slug:slug>", views.post, name="post-page"),
 ]
